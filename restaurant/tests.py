1841eb493384aa22c8c6fb9422c53a11e9ac997d
Roman Vlasenko <klavionik@gmail.com>
2020-07-09 15:56:02 +0500

    Update API endpoint test

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,12 +1,17 @@
-from rest_framework.test import APITestCase
-from restaurant.models import Category, Allergen
+from io import BytesIO
+from tempfile import TemporaryDirectory
+
+from PIL import Image
 from django.conf import settings
 from rest_framework.reverse import reverse
-import os
+from rest_framework.test import APITestCase, override_settings
 
-FIXTURES_DIR = os.path.join(settings.BASE_DIR, 'restaurant', 'fixtures')
+from restaurant.models import Category, Allergen
+
+FAKE_MEDIA_ROOT = TemporaryDirectory()
 
 
+@override_settings(MEDIA_ROOT=FAKE_MEDIA_ROOT.name)
 class CreateMenuItemTest(APITestCase):
 
     @classmethod
@@ -19,15 +24,25 @@ class CreateMenuItemTest(APITestCase):
     def test_create_menuitem(self):
         path = reverse('menu-items')
         token_header = {'HTTP_X_ACCESS_TOKEN': settings.ACCESS_TOKEN}
-        with open(os.path.join(FIXTURES_DIR, 'menu_image', 'carribeansaladshrimp.jpg'), 'rb') as fh:
+
+        with BytesIO() as buffer:
             payload = {
                 'name': 'Карибский салат с креветками',
-                'image': fh,
+                'image': self.get_test_img(buffer),
                 'category': self.salads.id,
                 'price': 800,
                 'calories': 600,
                 'allergens': [self.eggs.id, self.soy.id, self.shellfish.id]
             }
+
             response = self.client.post(path, payload, **token_header)
 
         self.assertEqual(response.status_code, 201, 'Блюдо добавлено')
+
+    @staticmethod
+    def get_test_img(buffer):
+        _image = Image.new('RGB', size=(1, 1), color=(0, 0, 0))
+        _image.save(buffer, 'JPEG')
+        buffer.name = 'carribeansaladshrimp.jpg'
+        buffer.seek(0)
+        return buffer
